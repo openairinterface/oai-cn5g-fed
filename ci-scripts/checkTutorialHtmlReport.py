openairinterface/oai-cn5g-fed

commit 3209b601e8bd85eb1448921ee266e8e65e351c8d

docs: tutorial readability update
- re-arrange the documents to improve readability
- remove old tutorials and files
- merge basic tutorial with basic static ip-address
and add duranta/oai gnb nrue based E2E testing
- update omec tutorial with latest omec v2.3 version

Signed-off-by: Sagar Arora <sagar.arora@openairinterface.org>

ci-scripts/Jenkinsfile-Tutorial-Check
@@ -328,7 +328,7 @@ pipeline {
           dir ('ci-scripts') {
             // Tee will make the command always pass
             // Please use the same log name as the folder used in the tutorial
-            sh './checkTutorial.py --tutorial DEPLOY_SA5G_BASIC_STATIC_UE_IP.md | tee ../archives/static-ue-ip.log'
+            sh './checkTutorial.py --tutorial DEPLOY_SA5G_BASIC_DEPLOYMENT.md | tee ../archives/static-ue-ip.log'
           }
           try {
             sh 'grep -l "is PASS" archives/static-ue-ip.log > /dev/null'

docker-compose/database/oai_db2.sql
@@ -321,7 +321,7 @@ INSERT INTO `SessionManagementSubscriptionData` (`ueid`, `servingPlmnid`, `singl
 INSERT INTO `SessionManagementSubscriptionData` (`ueid`, `servingPlmnid`, `singleNssai`, `dnnConfigurations`) VALUES
 ('208950000000035', '20895', '{\"sst\": 222, \"sd\": \"00007B\"}','{\"default\":{\"pduSessionTypes\":{ \"defaultSessionType\": \"IPV4\"},\"sscModes\": {\"defaultSscMode\": \"SSC_MODE_1\"},\"5gQosProfile\": {\"5qi\": 6,\"arp\":{\"priorityLevel\": 1,\"preemptCap\": \"NOT_PREEMPT\",\"preemptVuln\":\"NOT_PREEMPTABLE\"},\"priorityLevel\":1},\"sessionAmbr\":{\"uplink\":\"100Mbps\", \"downlink\":\"100Mbps\"},\"staticIpAddress\":[{\"ipv4Addr\": \"12.1.1.10\"}]}}');
 INSERT INTO `SessionManagementSubscriptionData` (`ueid`, `servingPlmnid`, `singleNssai`, `dnnConfigurations`) VALUES
-('208950000000036', '20895', '{\"sst\": 1, \"sd\": \"FFFFFF\"}','{\"oai\":{\"pduSessionTypes\":{ \"defaultSessionType\": \"IPV4\"},\"sscModes\": {\"defaultSscMode\": \"SSC_MODE_1\"},\"5gQosProfile\": {\"5qi\": 6,\"arp\":{\"priorityLevel\": 1,\"preemptCap\": \"NOT_PREEMPT\",\"preemptVuln\":\"NOT_PREEMPTABLE\"},\"priorityLevel\":1},\"sessionAmbr\":{\"uplink\":\"100Mbps\", \"downlink\":\"100Mbps\"}}}');
+('208950000000036', '20895', '{\"sst\": 1, \"sd\": \"FFFFFF\"}','{\"oai\":{\"pduSessionTypes\":{ \"defaultSessionType\": \"IPV4\"},\"sscModes\": {\"defaultSscMode\": \"SSC_MODE_1\"},\"5gQosProfile\": {\"5qi\": 6,\"arp\":{\"priorityLevel\": 1,\"preemptCap\": \"NOT_PREEMPT\",\"preemptVuln\":\"NOT_PREEMPTABLE\"},\"priorityLevel\":1},\"sessionAmbr\":{\"uplink\":\"100Mbps\", \"downlink\":\"100Mbps\"},\"staticIpAddress\":[{\"ipv4Addr\": \"12.1.1.130\"}]}}');
 INSERT INTO `SessionManagementSubscriptionData` (`ueid`, `servingPlmnid`, `singleNssai`, `dnnConfigurations`) VALUES
 ('208950000000037', '20895', '{\"sst\": 222, \"sd\": \"00007B\"}','{\"default\":{\"pduSessionTypes\":{ \"defaultSessionType\": \"IPV4\"},\"sscModes\": {\"defaultSscMode\": \"SSC_MODE_1\"},\"5gQosProfile\": {\"5qi\": 6,\"arp\":{\"priorityLevel\": 1,\"preemptCap\": \"NOT_PREEMPT\",\"preemptVuln\":\"NOT_PREEMPTABLE\"},\"priorityLevel\":1},\"sessionAmbr\":{\"uplink\":\"100Mbps\", \"downlink\":\"100Mbps\"}}}');
 INSERT INTO `SessionManagementSubscriptionData` (`ueid`, `servingPlmnid`, `singleNssai`, `dnnConfigurations`) VALUES

docker-compose/docker-compose-oai-rfsim-basic.yaml
@@ -1,0 +1,55 @@
+# SPDX-License-Identifier: MIT
+
+services:
+    oai-gnb-basic:
+        image: oaisoftwarealliance/oai-gnb:develop
+        container_name: oai-gnb-basic
+        cap_add:
+            - SYS_NICE
+        cap_drop:
+            - ALL
+        environment:
+            USE_ADDITIONAL_OPTIONS: -E --rfsim --MACRLCs.[0].stats_max_ue 0 --log_config.global_log_options level,nocolor,time
+            ASAN_OPTIONS: detect_leaks=0
+        volumes:
+            - ./ran-conf/gnb.conf:/opt/oai-gnb/etc/gnb.conf
+        networks:
+            public_net:
+                ipv4_address: 192.168.70.153
+        healthcheck:
+            test: /bin/bash -c "pgrep nr-softmodem"
+            interval: 10s
+            timeout: 5s
+            retries: 5
+
+    oai-nr-ue-basic:
+        image: oaisoftwarealliance/oai-nr-ue:develop
+        container_name: oai-nr-ue-basic
+        cap_drop:
+            - ALL
+        cap_add:
+            - NET_ADMIN  # for interface bringup
+            - NET_RAW    # for ping
+            - SYS_NICE
+        environment:
+            USE_ADDITIONAL_OPTIONS: -E --rfsim -r 106 --numerology 1 -C 3619200000 --rfsimulator.[0].serveraddr 192.168.70.153 --log_config.global_log_options level,nocolor,time
+            ASAN_OPTIONS: detect_leaks=0
+        depends_on:
+            - oai-gnb-basic
+        devices:
+             - /dev/net/tun:/dev/net/tun
+        volumes:
+            - ./ran-conf/nr-ue.conf:/opt/oai-nr-ue/etc/nr-ue.conf
+        networks:
+            public_net:
+                ipv4_address: 192.168.70.154
+        healthcheck:
+            test: /bin/bash -c "pgrep nr-uesoftmodem"
+            interval: 10s
+            timeout: 5s
+            retries: 5
+
+networks:
+    public_net:
+        name: demo-oai-public-net
+        external: true

docker-compose/docker-compose-omec-gnbsim.yaml
@@ -1,23 +1,17 @@
 # SPDX-License-Identifier: MIT
 
 services:
-    omec-gnbsim-vpp:
-        container_name: omec-gnbsim-vpp
-        image: 5gc-gnbsim:0.0.1-dev
+    omec-gnbsim:
+        container_name: omec-gnbsim
+        image: oaisoftwarealliance/omec-gnbsim:v2.3-fixes
         privileged: true
         command: bash -c "./gnbsim --cfg gnbsim.yaml && sleep infinity"
-        #command: sleep infinity
         networks:
             public_net:
                 ipv4_address: 192.168.70.171
-            public_net_access:
-                ipv4_address: 192.168.72.171
         volumes:
             - ./omec-gnbsim-config.yaml:/gnbsim/bin/gnbsim.yaml
 networks:
     public_net:
         name: demo-oai-public-net
-        external: True
-    public_net_access:
-        name: oai-public-access
         external: True

docker-compose/omec-gnbsim-config.yaml
@@ -10,7 +10,7 @@ info:
   description: GNBSIM initial local configuration
 
 configuration:
-  singleInterface: false #default value
+  singleInterface: true # the basic deployment has a single network, so N2 and N3 share it
   execInParallel: false #run all profiles in parallel
   runConfigProfilesAtStart: true
   httpServer: # Serves APIs to create/control profiles on the go
@@ -19,17 +19,16 @@ configuration:
     port: 8080
   gnbs: # pool of gNodeBs
     gnb1:
-      n2IpAddr: # gNB N2 interface IP address used to connect to AMF 
+      n2IpAddr: 192.168.70.171 # gNB N2 interface IP address used to connect to AMF, pinned in docker-compose-omec-gnbsim.yaml
       n2Port: 9487 # gNB N2 Port used to connect to AMF
-      n3IpAddr: 192.168.72.171 # gNB N3 interface IP address used to connect to UPF. when singleInterface mode is false
-      #n3IpAddr: "POD_IP" # when gnb is deployed in singleInterface mode
+      n3IpAddr: 192.168.70.171 # same address as N2: the basic deployment has one network only
       n3Port: 2152 # gNB N3 Port used to connect to UPF
       name: gnb1 # gNB name that uniquely identify a gNB within application
       globalRanId:
         plmnId:
           mcc: 208 # Mobile Country Code (3 digits string, digit: 0~9)
           mnc: 95 # Mobile Network Code (2 or 3 digits string, digit: 0~9)
-        gNbId: 
+        gNbId:
           bitLength: 24
           gNBValue: 000102 # gNB identifier (3 bytes hex string, range: 000000~FFFFFF)
       supportedTaList:
@@ -91,7 +90,7 @@ configuration:
       gnbName: gnb1 # gNB to be used for this profile
       startImsi: 208950000000031
       ueCount: 5
-      defaultAs: "192.168.73.135" #default icmp pkt destination
+      defaultAs: "192.168.70.135" #default icmp pkt destination
       opc: "63bfa50ee6523365ff14c1f45f88737d"
       key: "0C0A34601D4F07677303652C0462535B"
       sequenceNumber: "16f3b3f70fc2"
@@ -109,7 +108,7 @@ configuration:
       gnbName: gnb1 # gNB to be used for this profile
       startImsi: 208950000000125 # First IMSI. Subsequent values will be used if ueCount is more than 1
       ueCount: 5 # Number of UEs for for which the profile will be executed
-      defaultAs: "192.168.73.135" #default icmp pkt destination
+      defaultAs: "192.168.70.135" #default icmp pkt destination
       opc: "63bfa50ee6523365ff14c1f45f88737d"
       key: "0C0A34601D4F07677303652C0462535B"
       sequenceNumber: "16f3b3f70fc2"
@@ -164,7 +163,7 @@ configuration:
       gnbName: gnb1 # gNB to be used for this profile
       startImsi: 208950000000125 # First IMSI. Subsequent values will be used if ueCount is more than 1
       ueCount: 5 # Number of UEs for for which the profile will be executed
-      defaultAs: "192.168.73.135" #default icmp pkt destination
+      defaultAs: "192.168.70.135" #default icmp pkt destination
       sequenceNumber: "16f3b3f70fc2"
       opc: "63bfa50ee6523365ff14c1f45f88737d"
       key: "0C0A34601D4F07677303652C0462535B"

commit 053fd6a10dc9d7090d595a902013a92e375df54e

fix(ci): archive the basic-deployment tutorial under its own name
  - The tutorial was to DEPLOY_SA5G_BASIC_DEPLOYMENT.md,
    whose result folder is /tmp/oai/basic-deployment,
    but left the log name and archive label as static-ue-ip.
  - Also drop the `|| true` that turned the missing folder into a pass.

Fixes: 3209b601e8bd — "docs: tutorial readability update"
Signed-off-by: Shubhika Garg <shubhika.garg@openairinterface.org>

ci-scripts/Jenkinsfile-Tutorial-Check
@@ -328,16 +328,16 @@ pipeline {
           dir ('ci-scripts') {
             // Tee will make the command always pass
             // Please use the same log name as the folder used in the tutorial
-            sh './checkTutorial.py --tutorial DEPLOY_SA5G_BASIC_DEPLOYMENT.md | tee ../archives/static-ue-ip.log'
+            sh './checkTutorial.py --tutorial DEPLOY_SA5G_BASIC_DEPLOYMENT.md | tee ../archives/basic-deployment.log'
           }
           try {
-            sh 'grep -l "is PASS" archives/static-ue-ip.log > /dev/null'
+            sh 'grep -l "is PASS" archives/basic-deployment.log > /dev/null'
             echo "Static UE IP Tutorial PASSED"
           } catch (Exception e) {
             currentBuild.result = 'FAILURE'
             echo "Static UE IP Tutorial FAILED"
           }
-          archivesArtifacts('docker-compose/docker-compose-basic-nrf.yaml', 'static-ue-ip')
+          archivesArtifacts('docker-compose/docker-compose-basic-nrf.yaml', 'basic-deployment')
           cleanUpDockerCompose('docker-compose/docker-compose-basic-nrf.yaml')
         }
       }
@@ -532,7 +532,7 @@ def updateDockerCompose(filename, lRanTag) {
 }
 
 def archivesArtifacts(filename,folder) {
-  sh 'cp -Rf /tmp/oai/' + folder + ' archives || true'
+  sh 'cp -Rf /tmp/oai/' + folder + ' archives'
   // Making a copy of the docker-compose for debugging
   sh 'cp ' + filename + ' archives/' + folder + ' || true'
 }

ci-scripts/checkTutorialHtmlReport.py
@@ -31,7 +31,7 @@ class HtmlReport():
 		with open(os.path.join(cwd, REPORT_NAME), 'w') as wfile:
 			wfile.write(generate_header(args))
 
-			tutorials = ['mini-gnbsim', 'static-ue-ip', 'slicing-with-nssf', 'ulcl-scenario', 'mongodb-test', 'upf-ebpf-gnbsim', 'qos-testing', 'ethernet-pdu-testing']
+			tutorials = ['mini-gnbsim', 'basic-deployment', 'slicing-with-nssf', 'ulcl-scenario', 'mongodb-test', 'upf-ebpf-gnbsim', 'qos-testing', 'ethernet-pdu-testing']
 			for tutorial in tutorials:
 				if not os.path.isfile(cwd + '/archives/' + tutorial + '.log'):
 					continue